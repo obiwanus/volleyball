6af360a588395af3febd84f140b4646972781839
Ivan Ivanov <ivan@ivanovs.info>
2014-10-23 21:12:28 +1300

    Another dummy

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pygame
-
+from pygame.locals import *
 
 pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Tank wars')
+
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
